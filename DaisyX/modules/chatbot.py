263e120cc4ff13188cdf4626a12b3c47941c3e03
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-08-22 10:54:07 +0700

    Update chatbot.py

--- DaisyX/modules/chatbot.py
+++ DaisyX/modules/chatbot.py
@@ -148,10 +148,10 @@ Chatbot utilizes the CoffeeHouse API and allows Senku to talk and provides a mor
 
 *Commands:* 
 *Admins only:*
- ✪ `/addchat`*:* Enables Chatbot mode in the chat.
- ✪ `/rmchat`*:* Disables Chatbot mode in the chat.
+ ✪ `/addchat`*:* Mengaktifkan chat bot di obrolant.
+ ✪ `/rmchat`*:* Menonaktifkan chat bot di obrolan.
 
-Reports bugs at @{SUPPORT_CHAT}
+Reports bugs at @siiniaja
 [Powered by CoffeeHouse](https://coffeehouse.intellivoid.net) from @Intellivoid
 """
 
@@ -172,7 +172,7 @@ dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
-__mod_name__ = "Chatbot 🙋‍♀️"
+__mod_name__ = "Chatbot"
 __command_list__ = ["addchat", "rmchat", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER,
